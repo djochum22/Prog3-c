2b95f979e12c5ae18cdeb2f7dc857ff86e7bb722
Douglas Jochum <djochum22@gmail.com>
2025-10-27 22:42:04 +0100

    aufgabe 1.3 done

---
+++ Pra2/a13.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int nums[20];
+    int input = 0;
+    int i = 0;
+    int j;
+    int k;
+
+    while ((scanf("%i", &input) != EOF) && i <= 19)
+    {
+        nums[i] = input;
+        i++;
+    }
+
+    for (j = 0; j <= i; j++)
+    {
+        for (k = j; k <= i; k++)
+        {
+            if (nums[j] > nums[k])
+            {
+                input = nums[j];
+                nums[j] = nums[k];
+                nums[k] = input;
+            }
+        }
+    }
+
+    for (j = 0; j <= i; j++)
+    {
+        printf("%6i\n", nums[j]);
+    }
+}
