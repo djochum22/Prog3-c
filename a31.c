1bba042a4f6874193341aa7ff24b4626c2255950
Douglas Jochum <djochum22@gmail.com>
2025-11-08 00:27:27 +0100

    aufgabe 4.1 done but insanely difficult already

--- a31.c
+++ a31.c
@@ -4,39 +4,46 @@
 
 int main(int argc, char *argv[])
 {
-    char tempName[25];
+    char tempName[26];
     int i = 0;
 
     struct Node
     {
-        char name[25];
+        char name[26];
         struct Node *next;
     };
 
-    struct Node *ptr, *temp; 
-    struct Node newNode;
-
+    struct Node *ptr, *temp, *newNode, *start;
+    
     ptr = NULL;
-    temp = (struct Node *)malloc(sizeof(struct Node));
+    temp = NULL;
+    newNode = NULL;
+    start = NULL;
 
     while (scanf("%25s", tempName) != EOF)
     {
-        strcpy(newNode.name, tempName);
+        newNode = (struct Node *)malloc(sizeof(struct Node));
+        strcpy(newNode->name, tempName);
+        newNode->next = NULL;
 
-        if (ptr = NULL)
+        if (ptr == NULL)
         {
-            ptr = (struct Node *)malloc(sizeof(struct Node));
-            *ptr = newNode;
+            ptr = newNode;
         }
         else
         {
+            if (ptr == NULL)
+            {
+                return 0;
+            }
+
             temp = ptr;
             while (temp->next != NULL)
             {
                 temp = temp->next;
             }
 
-            temp->next = &newNode;
+            temp->next = newNode;
         }
     }
 
@@ -46,19 +53,28 @@ int main(int argc, char *argv[])
         temp = temp->next;
     }
 
-    temp->next = ptr->next;
+    temp->next = ptr;
 
     temp = ptr;
-    while (strcmp(argv[i], "\0"))
+
+    for (i = 2; i < argc; i++)
     {
         temp = temp->next;
-        i++;
     }
 
     printf("%s is dran!", temp->name);
 
+    temp = ptr->next;
+    
+    while (temp != ptr)
+    {
+        start = temp->next;
+        free(temp);
+        temp = start;
+    }
+
     free(ptr);
-    free(temp);
+
     ptr = NULL;
     temp = NULL;
     
