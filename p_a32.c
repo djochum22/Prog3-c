d1dbd29a8bc33bfa25dc5cdb5e5acc81daad42e6
Douglas Jochum <djochum22@gmail.com>
2026-01-17 11:29:54 +0100

    question a32 done all the way

--- p_a32.c
+++ p_a32.c
@@ -12,14 +12,22 @@ typedef struct Node *nodep;
 
 void printList(nodep lst);
 nodep insertAt(nodep lst, int pos, char *inhalt);
+nodep copyList(nodep lst);
+void deleteList(nodep lst);
 
 int main(void)
 {
     nodep lst = NULL;
+    nodep lst2 = NULL;
     lst = insertAt(lst, 0, "hi");
     lst = insertAt(lst, 0, "hi new");
     lst = insertAt(lst, -1, "Hi Last");
+    lst2 = copyList(lst);
+
     printList(lst);
+
+    deleteList(lst2);
+    deleteList(lst);
     return 0;
 }
 
@@ -37,7 +45,7 @@ nodep insertAt(nodep lst, int pos, char *inhalt)
 {
     int i = 0;
     nodep temp = lst;
-    nodep new_node = (nodep)malloc(sizeof(nodep));
+    nodep new_node = malloc(sizeof(struct Node));
 
     new_node->inhalt = inhalt;
 
@@ -79,6 +87,7 @@ nodep insertAt(nodep lst, int pos, char *inhalt)
 
         if (temp == NULL)
         {
+            free(new_node);
             return lst;
         }
 
@@ -90,4 +99,58 @@ nodep insertAt(nodep lst, int pos, char *inhalt)
     }
 
     return lst;
+}
+
+nodep copyList(nodep lst)
+{
+    nodep new_node, head, tail; 
+
+    if (lst == NULL)
+    {
+        return NULL;
+    }
+
+    head = NULL;
+    tail = NULL;
+
+    while (lst != NULL)
+    {
+        new_node = malloc(sizeof(struct Node));
+        new_node->inhalt = lst->inhalt;
+        new_node->next = NULL;
+        new_node->prev = tail;
+
+        if (tail != NULL)
+        {
+            tail->next = new_node;
+        }
+        else
+        {
+            head = new_node;
+        }
+
+        tail = new_node;
+        lst = lst->next;
+    }
+
+    return head;
+}
+
+void deleteList(nodep lst)
+{
+    nodep temp;
+
+    if (lst == NULL)
+    {
+        return;
+    }
+    
+    temp = lst;
+
+    while (temp != NULL)
+    {
+        temp = temp->next;
+        free(lst);
+        lst = temp;
+    }
 }
